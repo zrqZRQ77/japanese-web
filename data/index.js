11ca97fafcba4febd6c75f961c21cbff93396ee8
zrqZRQ77 <57594531+zrqZRQ77@users.noreply.github.com>
2026-06-13 14:05:36 +0800

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,10 +1,11 @@
 // @ts-ignore
 import boki3_ch1 from './boki3_ch1.json';
 
+const dataMap = {
+  'boki3_ch1': boki3_ch1,
+};
+
 export const getChapterData = (courseId, chapterId) => {
   const key = `${courseId}_${chapterId}`;
-  const dataMap = {
-    'boki3_ch1': boki3_ch1,
-  };
   return dataMap[key] || null;
 };
